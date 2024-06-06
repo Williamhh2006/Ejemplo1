cbc3f43ab5cd1e6ecef4e6098e44fb67484bf1a4
Williamhh2006 <2023-40106@liceocanadiense.edu.gt>
2024-06-06 14:12:20 -0600

    se van a subir 3 proyecto

---
+++ ejemplo1.py
@@ -0,0 +1,13 @@
+a=5
+b=8
+c=100
+d=10
+e=12
+s=a+b+c+d+e
+r=c-a-b-d-e
+m=a*b*c*d*e
+di=c/12/10/8/5
+print ("la suma es de", s)
+print ("la resta es de", r)
+print ("la multiplicación es de", m)
+print ("la división es de", di)
